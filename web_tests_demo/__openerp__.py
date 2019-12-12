92068ed45dc7fd77b9bb42575411e749743aa69e
Murtuza Saleh(SerpentCS) <m.saleh.serpentcs@gmail.com>
2019-12-12 16:04:26 +0530

    Update __openerp__.py

--- web_tests_demo/__openerp__.py
+++ web_tests_demo/__openerp__.py
@@ -7,9 +7,11 @@ OpenERP Web demo of a test suite
 
 Test suite example, same code as that used in the testing documentation.
     """,
+    'licence': 'AGPL-3',
     'depends': ['web'],
     'data' : [
         'views/web_tests_demo.xml',
     ],
     'qweb': ['static/src/xml/demo.xml'],
 }
+'
